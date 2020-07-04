6380bc44d938b6d24613730a326d8e989fd09eae
Tanosimboangy <jacquit.aja@onja.org>
2020-07-04 11:58:09 +0300

    adding id

--- script.js
+++ script.js
@@ -29,44 +29,41 @@ thirdDiv.appendChild(secondDiv);
 thirdDiv.setAttribute("id", "post-list");
 console.log(thirdDiv);
 
+
+
 // Accessing postTitle elemnst from the HTML.
 const postTitle = document.querySelector("#new-post-title");
-console.log(postTitle);
 postTitle.addEventListener('change', ($event) => {
 	newH5.textContent = $event.target.value;
 	});
 
 // Accessing postContent elemnst from the HTML.
 const postContent = document.querySelector("#new-post-content");
-console.log(postContent);
 postContent.addEventListener('change', ($event) => {
 	newParagraph.textContent = $event.target.value;
 	});
 
 // Accessing hobbiesTag elemnst from the HTML.
 const hobbiesTag = document.querySelector("#hobbies-tag");
-console.log(hobbiesTag);
 hobbiesTag.addEventListener('change', ($event) => {
 	newSpan.textContent = $event.target.value;
 });
 
 // Accessing schoolLifeTag elemnst from the HTML.
 const schoolLifeTag = document.querySelector("#school-life-tag");
-console.log(schoolLifeTag);
 schoolLifeTag.addEventListener('change', ($event) => {
 	newSpan.textContent = $event.target.value;
 	});
 
 // Accessing personalTag elemnst from the HTML.
 const personalTag = document.querySelector("#personal-tag");
-console.log(personalTag);
 personalTag.addEventListener('change', ($event) => {
 	newSpan.textContent = $event.target.value;
 	});
 
 // Accessing submitForm elemnst from the HTML.
 const submitForm = document.querySelector("#submit-form");
-console.log(submitForm);
-submitForm.addEventListener('change', ($event) => {
-	thirdDiv.textContent = $event.target.value;
+submitForm.addEventListener('click', ($event) => {
+	personalTag.textContent = $event.target.value;
 });
+console.log(submitForm);
