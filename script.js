4bf29c77d9b078fc8f6138ccbb1bf61d450f1096
Tanosimboangy <jacquit.aja@onja.org>
2020-07-04 11:35:05 +0300

    accessing the necessary elements from the HTML

--- script.js
+++ script.js
@@ -1,12 +1,64 @@
+// Creating a h5 element for the new post.
 const newH5 = document.createElement("h5");
 newH5.classList.add("new-card-title");
-console.log(newH5);
+// Creating a new paragraph element for the new post.
 const newParagraph = document.createElement("p");
 newParagraph.classList.add("new-card-text");
-console.log(newParagraph);
+// Creating new span element for the new post.
 const newSpan = document.createElement("span");
 newSpan.classList.add("new-badge", "new-badge-light");
-console.log(newSpan);
-const firsDiv = document.createElement("div");
-firsDiv.classList.add("new-card-body");
-console.log(firsDiv);
+// Creating first div element for the new post.
+const firstDiv = document.createElement("div");
+firstDiv.classList.add("new-card-body");
+firstDiv.appendChild(newH5);
+firstDiv.appendChild(newParagraph);
+firstDiv.appendChild(newSpan);
+// Creating second div element for the new post.
+const secondDiv = document.createElement("div");
+secondDiv.classList.add("card", "some-space");
+secondDiv.appendChild(firstDiv);
+// Creating third div element for the new post.
+const thirdDiv = document.createElement("div");
+thirdDiv.classList.add("col-8", "offset-2");
+thirdDiv.appendChild(secondDiv);
+console.log(thirdDiv);
+// id="post-list">
+
+// Accessing postTitle elemnst from the HTML.
+const postTitle = document.querySelector("#new-post-title");
+console.log(postTitle);
+postTitle.addEventListener('change', ($event) => {
+	newH5.textContent = $event.target.value;
+	});
+// Accessing postContent elemnst from the HTML.
+const postContent = document.querySelector("#new-post-content");
+console.log(postContent);
+postContent.addEventListener('change', ($event) => {
+	newParagraph.textContent = $event.target.value;
+	});
+// Accessing hobbiesTag elemnst from the HTML.
+const hobbiesTag = document.querySelector("#hobbies-tag");
+console.log(hobbiesTag);
+hobbiesTag.addEventListener('change', ($event) => {
+	newSpan.textContent = $event.target.value;
+});
+// Accessing schoolLifeTag elemnst from the HTML.
+const schoolLifeTag = document.querySelector("#school-life-tag");
+console.log(schoolLifeTag);
+schoolLifeTag.addEventListener('change', ($event) => {
+	newSpan.textContent = $event.target.value;
+	});
+// Accessing personalTag elemnst from the HTML.
+const personalTag = document.querySelector("#personal-tag");
+console.log(personalTag);
+personalTag.addEventListener('change', ($event) => {
+	newSpan.textContent = $event.target.value;
+	});
+
+
+
+// const submitForm = document.querySelector("#submit-form");
+// console.log(submitForm);
+// submitForm.addEventListener('change', ($event) => {
+// 	postList.textContent = $event.target.value;
+// });
