ca3745836e33c724c9284e70cbcaf5377c861dd1
Tanosimboangy <jacquit.aja@onja.org>
2020-07-04 17:26:57 +0300

    finishing  the first part

--- script.js
+++ script.js
@@ -10,6 +10,11 @@ newPostContent.classList.add("new-card-text");
 const newSpan = document.createElement("span");
 newSpan.classList.add("new-badge", "new-badge-light");
 
+const hobbiesTag = document.
+getElementById("hobbies-tag");
+const schoolLifeTag = document.getElementById("school-life-tag");
+const personalTag = document.getElementById("personal-tag");
+
 // Creating first div element for the new post.
 const firstDiv = document.createElement("div");
 firstDiv.classList.add("new-card-body");
@@ -25,7 +30,6 @@ secondDiv.appendChild(firstDiv);
 const postList = document.getElementById("post-list");
 postList.appendChild(secondDiv);
 
-
 const postTitle = document.getElementById("new-post-title");
 const postContent = document.getElementById("new-post-content");
 
@@ -37,5 +41,6 @@ submitForm.addEventListener('click', ($event) => {
 	$event.preventDefault();
 	newPostTitle.textContent = `${postTitle.value}`;
 	newPostContent.textContent = `${postContent.value}`;
+	newSpan.textContent = `${hobbiesTag.value}`;
 	form.reset();
 });
