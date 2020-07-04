829a2ec0b5a3509e7e799093e61372d96e444d85
Tanosimboangy <jacquit.aja@onja.org>
2020-07-04 16:32:51 +0300

    calling the div

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Creating a h5 element for the new post.
-const newH5 = document.createElement("h5");
-newH5.classList.add("new-card-title");
+const newPostTitle = document.createElement("h5");
+newPostTitle.classList.add("new-card-title");
 
 // Creating a new paragraph element for the new post.
-const newParagraph = document.createElement("p");
-newParagraph.classList.add("new-card-text");
+const newPostContent = document.createElement("p");
+newPostContent.classList.add("new-card-text");
 
 // Creating new span element for the new post.
 const newSpan = document.createElement("span");
@@ -13,8 +13,8 @@ newSpan.classList.add("new-badge", "new-badge-light");
 // Creating first div element for the new post.
 const firstDiv = document.createElement("div");
 firstDiv.classList.add("new-card-body");
-firstDiv.appendChild(newH5);
-firstDiv.appendChild(newParagraph);
+firstDiv.appendChild(newPostTitle);
+firstDiv.appendChild(newPostContent);
 firstDiv.appendChild(newSpan);
 
 // Creating second div element for the new post.
@@ -22,48 +22,20 @@ const secondDiv = document.createElement("div");
 secondDiv.classList.add("card", "some-space");
 secondDiv.appendChild(firstDiv);
 
-// Creating third div element for the new post.
-const thirdDiv = document.createElement("div");
-thirdDiv.classList.add("col-8", "offset-2");
-thirdDiv.appendChild(secondDiv);
-thirdDiv.setAttribute("id", "post-list");
-console.log(thirdDiv);
+const postList = document.getElementById("post-list");
+postList.appendChild(secondDiv);
 
 
+const postTitle = document.getElementById("new-post-title");
+const postContent = document.getElementById("new-post-content");
 
-// Accessing postTitle elemnst from the HTML.
-const postTitle = document.querySelector("#new-post-title");
-postTitle.addEventListener('change', ($event) => {
-	newH5.textContent = $event.target.value;
-	});
-
-// Accessing postContent elemnst from the HTML.
-const postContent = document.querySelector("#new-post-content");
-postContent.addEventListener('change', ($event) => {
-	newParagraph.textContent = $event.target.value;
-	});
-
-// Accessing hobbiesTag elemnst from the HTML.
-const hobbiesTag = document.querySelector("#hobbies-tag");
-hobbiesTag.addEventListener('change', ($event) => {
-	newSpan.textContent = $event.target.value;
-});
-
-// Accessing schoolLifeTag elemnst from the HTML.
-const schoolLifeTag = document.querySelector("#school-life-tag");
-schoolLifeTag.addEventListener('change', ($event) => {
-	newSpan.textContent = $event.target.value;
-	});
-
-// Accessing personalTag elemnst from the HTML.
-const personalTag = document.querySelector("#personal-tag");
-personalTag.addEventListener('change', ($event) => {
-	newSpan.textContent = $event.target.value;
-	});
+const form = document.querySelector("form");
 
 // Accessing submitForm elemnst from the HTML.
 const submitForm = document.querySelector("#submit-form");
 submitForm.addEventListener('click', ($event) => {
-	personalTag.textContent = $event.target.value;
+	$event.preventDefault();
+	newPostTitle.textContent = `${postTitle.value}`;
+	newPostContent.textContent = `${postContent.value}`;
+	form.reset();
 });
-console.log(submitForm);
